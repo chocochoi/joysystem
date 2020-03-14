4a660f6ebe176381364f5fe73f8f3da937e74981
chocochoi <chocochoi76@gmail.com>
2020-03-14 23:38:02 +0900

    eslint,prettier 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      아빠 안녕!!!!!!!!!!!!!!! ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
-    </div>
+    <div id="wrap">아빠 안녕!!!!!!!!!!!!!!! ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</div>
   );
 }
 
