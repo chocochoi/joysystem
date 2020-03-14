b5917e85092a86b1ff682fec113edf62aef11627
chocochoi <chocochoi76@gmail.com>
2020-03-14 22:46:54 +0900

    초기세팅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      아빠 안녕!!!!!!!!!!!!!!! ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+    </div>
+  );
+}
+
+export default App;
