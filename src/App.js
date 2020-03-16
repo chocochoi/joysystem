63f1236d31b77555473f509ff2a7e3eb51527fe6
chocochoi <chocochoi@stunitas.com>
2020-03-16 20:03:24 +0900

    bable-eslint 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { NotFound } from './components/Errors';
+// import { NotFound } from './components/Errors';
 import Loading from './components/Loading';
 import Header from './components/Header';
 import './App.css';
@@ -11,7 +11,7 @@ const Sub01 = lazy(() => import('./containers/Sub01'));
 function App() {
   return (
     <BrowserRouter basename="/">
-      <Header/>
+      <Header />
       <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path="/main" component={Main} />
